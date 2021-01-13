c8c8bcf729e1f23d4eb645874d6652431d21f62b
ibrahni <diihnini@gmail.com>
2021-01-14 00:45:27 +0100

    use svg instead of canvas for hourly weather display

--- src/component/HourlyWeatherComponent/HourlyWeatherComponent.js
+++ src/component/HourlyWeatherComponent/HourlyWeatherComponent.js
@@ -3,45 +3,77 @@ import  React  from "react";
 import Card from 'react-bootstrap/Card'
 import HourlyWeather from './data.json'
 class HourlyWeatherComponent extends React.Component {
-    
-    componentDidMount(){
-        this.drawHourlyWeather();
+    constructor(props){
+        super(props);
+        this.state = {
+            'hourly' : []
+        }
     }
-    componentDidUpdate(){
-        this.drawHourlyWeather();
 
+    componentDidMount(){
+        this.setState({
+            'hourly' : this.drawHourlySVG(),
+            'path' : "M" + this.drawHourlySVG().map((element) => element.x+" "+element.y).join(" L")
+        });
     }
-    drawHourlyWeather(){
-        const canvas = this.refs.canvas;
-        const ctx = canvas.getContext("2d");
-        const clientRectangle = canvas.getBoundingClientRect();
-        const step = (clientRectangle.width - 20)/HourlyWeather.length
-        const height = clientRectangle.height;
+
+
+
+    drawHourlySVG(){
+        const svg = this.refs.hourlySVG;
+        const height = svg.clientHeight;
+        const width = svg.clientWidth;
+
+        const step = width/(HourlyWeather.length+1)
         let axis = HourlyWeather.map(element => element.temperature.now).sort((a,b) => parseInt(a) - parseInt(b))
         let axisStep = axis[axis.length-1] - axis[0]   
+        let fff = axisStep - axis[0];
+        let starter = fff < (height)/2 ? height/2 :  0
+        const hourly = HourlyWeather.map((element, key) => {
+            return this.constructAPoint(starter, axisStep, element, step*key+step, height);
+        });
+        return hourly
+    }
 
-        HourlyWeather.forEach((element, key) => {
-            this.drawTemperature((height-10)/(axisStep+1), ctx, axisStep, element, step*key, height);
-        })
+    constructAPoint(starter, axisStep, element , x, y){
+        return {
+            "x" : x,
+            "y" : starter+axisStep-element.temperature.now,
+            "temperature" : element.temperature.now,
+            "hour": element.dt+":00"
+        }
     }
     
-    drawTemperature(bottom, ctx, axisStep, element , x, y){
-        ctx.fillText(element.temperature.now, x, (bottom*(axisStep-element.temperature.now)))
-
-        ctx.lineTo(x, (bottom*(axisStep-element.temperature.now)))
-        ctx.stroke();
-
-        ctx.fillText(element.dt+":00", x, y);
-    }
-
     render(){
         return  (
             <Card id="hourly-display" bg="dark" >
                 <Card.Header  className="text-center">
-                            <h1>UpComig Hours</h1>
+                            <h4>UpComig Hours</h4>
                 </Card.Header>
                 <Card.Body>
-                    <canvas ref="canvas" id="canvas"/>
+                    <svg ref="hourlySVG" id="hourly-svg">
+                        <g font-size="15" font-family="sans-serif" fill="black" stroke="none" text-anchor="middle">
+                        {
+                            this.state.hourly && this.state.hourly.map((element, key) => {
+                              return ( <text x={element.x} y={element.y} dy="-10" dx="-3">{element.temperature}</text>)
+                            })
+                        }
+                        </g>
+                        <g fill="none" font-size="10" font-family="sans-serif" text-anchor="middle">
+
+                            {
+                                this.state.hourly.map((element, key) => {
+                                    return (
+                                        <g class="tick" opacity="1" transform={`translate(${element.x},130)`}>
+                                            <text fill="currentColor" y="10" dy="0.71em">{element.hour}</text>
+                                        </g>
+                                    )
+                                })
+                            }
+                        </g>
+                        <path d={this.state.path+"L442 200 L55 200 S"} fill="rgba(255, 204, 0, 0.2)"  />
+                        <path d={this.state.path} stroke="#fc0" fill="none" stroke-width="2"/>
+                    </svg>
                 </Card.Body>
             </Card>
         )
